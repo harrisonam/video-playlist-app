bd6243202b8293922442efdd32c55a4155a2f4d1
MuhammadMurtza <work.murtza@gmail.com>
2024-07-22 16:49:35 +0500

    add navbar, sidebar and apply bootsrap styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    case flash_type
+    when 'notice'
+      'alert-info'
+    when 'alert'
+      'alert-danger'
+    else
+      'alert-info'
+    end
+  end
 end
